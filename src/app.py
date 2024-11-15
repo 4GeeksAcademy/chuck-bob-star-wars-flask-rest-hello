a683687fe3ee43d2fac30a07bbf8a2309869a8f0
Chuck-Bob <derekaassaf@gmail.com>
2024-11-15 21:04:35 +0000

    first

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Favorite, People, Planets 
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,100 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/user', methods=['GET'])                        #USER GET
+def handle_hello_user():
+    users = User.query.all()        # Query all users from the database
+    response_body = [               # Format users into a list of dictionaries
+        {"id": user.id, 
+         "email": user.email, 
+         "is_active": user.is_active}
+        for user in users
+    ]
+    return jsonify(response_body), 200  # Return the response
 
+@app.route('/user', methods=['POST'])                  #USER POST
+def add_new_user():
+    request_body = request.json
+    email = request_body.get("email")
+    password = request_body.get("password")
+    is_active = request_body.get("is_active", True) 
+    user = User(email=email, password=password, is_active=is_active)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"message": "User added successfully", "user": {"email": email, "is_active": is_active}}), 201
+
+
+@app.route('/user/<int:user_id>', methods=['DELETE'])  #USER DELETE    
+def delete_user(user_id):
+    user = User.query.get(user_id)                  # Fetch the user by ID
+    db.session.delete(user)                         # Delete the user from the database
+    db.session.commit()                             # Commit the deletion
+    return jsonify([{"id": user.id, "email": user.email} for user in User.query.all()]), 200  # Return the updated list of users
+
+
+
+
+@app.route('/user/favorites', methods=['GET'])              #GET USER FAVORITE
+def handle_hello_favorites():
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "Hello, this is your GET /user/favorites response "
     }
-
     return jsonify(response_body), 200
+
+
+
+@app.route('/people', methods=['GET'])                      #GET ALL PEOPLE
+def get_all_people():
+    all_people = People.query.all()  # Fetch all records from the People table
+    all_people = list(map(lambda x: x.serialize(), all_people))
+    return jsonify(all_people), 200  # Return the list of people as a JSON response
+
+
+@app.route('/people/<int:people_id>', methods=['GET'])      # GET request for a single PERSON
+def get_single_person(people_id):
+    person = People.query.get(people_id)                    # Fetch a single person by their ID
+    response_body = {                                       # Convert the person object to a dictionary
+        "id": person.id,                                    # Include the person's ID
+        "name": person.name,                                # Include the person's name
+        "url": person.url                                   # Include the person's URL
+    }
+    return jsonify(response_body), 200                      # Return the person's data as a JSON response
+
+@app.route('/people/<int:person_id>', methods=['DELETE'])  #PERSON DELETE    
+def delete_person(person_id):
+    person = person.query.get(person_id)                  # Fetch the PERSON by ID
+    db.session.delete(person)                         # Delete the person from the database
+    db.session.commit()                             # Commit the deletion
+    return jsonify([{"id": person.id, "email": person.email} for person in person.query.all()]), 200  # Return the updated list of persons
+
+
+
+
+
+
+
+
+@app.route('/planets', methods=['GET'])                      #GET ALL PLANETS
+def get_all_planets():
+    all_planets = Planets.query.all()  # Fetch all records from the People table
+    all_planets = list(map(lambda x: x.serialize(), all_planets))
+    return jsonify(all_planets), 200  # Return the list of people as a JSON response
+
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])      # GET request for a single PLANET
+def get_single_planet(planet_id):
+    planet = People.query.get(planet_id)                    # Fetch a single planet by their ID
+    response_body = {                                       # Convert the planet object to a dictionary
+        "id": planet.id,                                    # Include the planet's ID
+        "name": planet.name,                                # Include the planet's name
+        "url": planet.url                                   # Include the planet's URL
+    }
+    return jsonify(response_body), 200                      # Return the person's data as a JSON response
+
+
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -17,3 +17,98 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+
+    def __repr__(self):
+        return '<Favorite %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "people_id": self.people_id,
+            "planets_id": self.planets_id
+            # do not serialize the password, its a security breach
+        }
+
+
+
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    url = db.Column(db.String(250), nullable=False)
+
+
+    def __repr__(self):
+        return '<People %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "url": self.url,
+        }
+
+
+    
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    url = db.Column(db.String(250), nullable=False)
+
+
+    def __repr__(self):
+        return '<People %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "url": self.url,
+        }
+
+
+
+
+
+
+
+    
+
+
+
+
+
+        # swapi_id = db.Column(db.String(250), nullable=False)
+    # mass = db.Column(db.Integer, nullable=False)
+    # hair_color = db.Column(db.String(250), nullable=False)
+    # skin_color = db.Column(db.String(250), nullable=False)
+    # eye_color = db.Column(db.String(250), nullable=False)
+    # birth_year = db.Column(db.String(250), nullable=False)
+    # gender = db.Column(db.String(250), nullable=False)
+    # created = db.Column(db.String(250), nullable=False)
+    # edited = db.Column(db.String(250), nullable=False)
+    # homeworld = db.Column(db.String(250), nullable=False)
+
+
+
+
+        # diameter = db.Column(db.Integer, nullable=False)
+    # rotation_period = db.Column(db.Integer, nullable=False)
+    # orbital_period = db.Column(db.Integer, nullable=False)
+    # gravity = db.Column(db.String(250), nullable=False)
+    # population = db.Column(db.Integer, nullable=False)
+    # climate = db.Column(db.String(250), nullable=False)
+    # terrain = db.Column(db.String(250), nullable=False)
+    # surface_water = db.Column(db.Integer, nullable=False)
+    # created = db.Column(db.String(250), nullable=False)
+    # edited = db.Column(db.String(250), nullable=False)
+    # name = db.Column(db.String(250), nullable=False)
+    # url = db.Column(db.String(250), nullable=False)
